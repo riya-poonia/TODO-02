67ebe8a8f4df938bb570debc699a467deb6dc64b
Riya Poonia <riyapoonia@Riyas-MacBook-Air.local>
2023-04-02 14:03:00 +0530

    Added about page, header and footer

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { request } = require("https");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 var items = ["Buy food", "Cook food", "Eat food"];
+var workItems = [];
 
 app.get("/", function (req, res) {
   var today = new Date();
@@ -19,19 +21,38 @@ app.get("/", function (req, res) {
   var day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     newListItems: items,
   });
 });
 
 app.post("/", function (req, res) {
-  //console.log(req.body.newItem);
+  //console.log(req.body);
   var item = req.body.newItem;
-  items.push(item);
-  //res.render("list", { newListItem: item });
-  res.redirect("/");
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
+app.get("/work", function (req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems,
+  });
+});
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 app.listen(4000, function () {
   console.log("Server started at port 4000");
 });
